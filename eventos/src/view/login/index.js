477f2d3837b22cde3815bcc93f7677543f0ceafd
zilioti <36432628+sh4ddy@users.noreply.github.com>
2020-12-08 16:05:08 -0300

    button

--- eventos/src/view/login/index.js
+++ eventos/src/view/login/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import './login.css'
 
 function Login(){
     return(
-        <h1>Tela de Login</h1>
+        <div className="login-content d-flex align-items-center">
+                <form className="form-signin mx-auto">
+                <div className="text-center mb-4">
+                    <h1 className="h3 mb-3 fw-normal">Login</h1>
+                    
+                    
+                    <div>
+                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" />
+                    </div>
+                    
+                    <div>    
+                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" />
+                    </div>
+                    
+                    <button className="w-100 btn btn-lg btn-block btn-login" type="submit">Sign in</button>
+                    
+                    <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+                </div>
+            </form>
+        </div>
+
     )
 }
 
