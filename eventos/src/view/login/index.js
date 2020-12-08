730d6c9df822bea95c2914a09da9964b7e6d438f
zilioti <36432628+sh4ddy@users.noreply.github.com>
2020-12-08 17:34:44 -0300

    Autenticação funcionando

--- eventos/src/view/login/index.js
+++ eventos/src/view/login/index.js
@@ -6,12 +6,13 @@ import 'firebase/auth';
 function Login(){
     const [email, setEmail] = useState();
     const [senha, setSenha] = useState();
+    const [msgTipo, setMsgTipo] = useState();
 
     function logar(){
         firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
-            alert('USUARIO LOGADO!');
+            setMsgTipo('sucesso');
         }).catch(error => {
-            alert('ERRO!');
+            setMsgTipo('erro');
         });
     }
 
@@ -27,9 +28,10 @@ function Login(){
                     <button onClick={logar} className="w-100 btn btn-lg btn-block btn-login" type="button">Logar</button>
                     
                     <div className="msg-login text-white my-5">
-                        <span><strong>WOW!</strong> Você esta conectado! &#128077; </span>
-                        <br></br>
-                        <span><strong>OPS!</strong> Verifique o Email e a Senha &#128078; </span>
+
+                        {msgTipo === 'sucesso' && <span><strong>WOW!</strong> Você esta conectado! &#128077; </span>}
+                        {msgTipo === 'erro' &&  <span><strong>OPS!</strong> Verifique o Email e a Senha &#128078; </span>}
+                        
                     </div>
 
                     <div className="opcoes-login mt-5">
